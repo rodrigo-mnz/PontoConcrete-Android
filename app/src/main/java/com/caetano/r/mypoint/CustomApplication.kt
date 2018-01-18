1d34b5c7c0f330cbb6d68756d1aa641977e96e0c
Rodrigo Caetano <rdg.mnz@gmail.com>
2018-01-18 18:46:39 -0200

    Implement JobScheduler to notify updates

---
+++ app/src/main/java/com/caetano/r/mypoint/CustomApplication.kt
@@ -0,0 +1,46 @@
+package com.caetano.r.mypoint
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+
+class CustomApplication : Application() {
+
+    companion object {
+        val UPDATE_CHANNEL_ID = "UPDATE_CHANNEL_ID"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            val channelName = "Updates"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val notificationChannel = NotificationChannel(UPDATE_CHANNEL_ID, channelName, importance)
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            val halfDayInMillis = 43200000L
+            jobScheduler.schedule(JobInfo.Builder(1,
+                    ComponentName(this, UpdateJob::class.java))
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                    .setMinimumLatency(halfDayInMillis)
+                    .build())
+        }
+    }
+}
--- app/src/main/java/com/caetano/r/mypoint/UpdateJob.kt
+++ app/src/main/java/com/caetano/r/mypoint/UpdateJob.kt
@@ -1,33 +1,33 @@
 package com.caetano.r.mypoint
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import android.util.Log
+import com.caetano.r.mypoint.CustomApplication.Companion.UPDATE_CHANNEL_ID
 import com.caetano.r.mypoint.api.ApiGithub
 import com.caetano.r.mypoint.api.GithubRelease
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class UpdateJob : JobService() {
-
-    private val updateChannel = "UPDATE"
 
     override fun onStopJob(p0: JobParameters?): Boolean {
         return false
     }
 
     override fun onStartJob(p0: JobParameters?): Boolean {
-        val mBuilder = NotificationCompat.Builder(this, updateChannel)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-
-
+        Log.i(this.toString(), "onStartJob")
         ApiGithub.service.getLatestRelease().enqueue(object : Callback<GithubRelease> {
             override fun onResponse(call: Call<GithubRelease>, response: Response<GithubRelease>) {
                 if (response.isSuccessful && response.body() != null) {
@@ -43,6 +43,25 @@ class UpdateJob : JobService() {
     }
 
     private fun notifyIfNeeded(githubRelease: GithubRelease) {
-        if (BuildConfig.VERSION_NAME)
+        val compare = githubRelease.tag_name.compareTo(BuildConfig.VERSION_NAME, true)
+        if (compare > 0) {
+
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(githubRelease.assets[0].browser_download_url))
+
+            val resultPendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    browserIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            val builder = NotificationCompat.Builder(this, UPDATE_CHANNEL_ID)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("PontoConcrete")
+                    .setContentText("Nova versão!")
+                    .setContentIntent(resultPendingIntent)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(1, builder.build())
+        }
     }
 }
