d1663d5407967659af6f9d7d8cfa140e9aea17d0
Rodrigo Caetano <rodrigo.caetano@concrete.com.br>
2018-03-20 11:32:31 -0300

    Add a 30 seconds timeout

--- app/src/main/java/com/caetano/r/mypoint/api/Api.kt
+++ app/src/main/java/com/caetano/r/mypoint/api/Api.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object Api {
 
@@ -19,6 +20,8 @@ object Api {
 
         val client = OkHttpClient.Builder()
                 .addInterceptor(logging)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
                 .build()
 
         val mRetrofit = Retrofit.Builder()
